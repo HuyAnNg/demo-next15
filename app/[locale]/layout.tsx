04ef2b2d554965e08ae8aef3534259e021040f81
nguyenhuyhoang241298 <nguyenhuyhoang241298@gmail.com>
2025-03-19 18:04:38 +0700

    feat(login): install sidebar

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from '@/components/providers/theme-provider'
 import { Toaster } from '@/components/ui/sonner'
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import './globals.css'
+import '../globals.css'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
